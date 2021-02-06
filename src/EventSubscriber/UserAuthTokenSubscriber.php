e702ae06232a61ed49c4df792ae8ab5a52e31d48
vs <vs@peytz.dk>
2021-02-06 23:06:27 +0200

    Main skeleton, install/uninstall, configs, redirect, token generation.

---
+++ src/EventSubscriber/UserAuthTokenSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\user_auth_token\EventSubscriber;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Url;
+use Drupal\user\Entity\User;
+
+class UserAuthTokenSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = array('checkAuth');
+    return $events;
+  }
+
+  /**
+   * Method to check token and ahtneticate user.
+   */
+  public function checkAuth($event) {
+
+    //$event->getRequest()->getQueryString();
+    //$event->getRequest()->getRequestUri();
+
+    $request = \Drupal::request();
+
+    $_ = $request->query->get('authtoken');
+    $auth_token = is_null($_) ? 'token_not_set' : $_;
+
+    if ($auth_token != '' && $auth_token == 'qwe123') {
+
+// TODO
+// - query field for token
+// - make autologin
+//     $user = User::load($uid);
+//     user_login_finalize($user);
+
+      $destination = trim(str_replace($request->getQueryString(), '', $request->getRequestUri()), '?');
+      $response = new RedirectResponse($destination);
+      $response->send();
+    }
+
+    drupal_set_message(t('@_1', ['@_1' => $auth_token]), 'error');
+
+    $event->stopPropagation();
+  }
+
+}
