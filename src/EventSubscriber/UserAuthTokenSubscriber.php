9398dc6a4ee5cf002e160cec0708d4fba304d0bf
vs <vs@peytz.dk>
2021-02-07 22:23:44 +0200

    Tokenized auth and redirect.

--- src/EventSubscriber/UserAuthTokenSubscriber.php
+++ src/EventSubscriber/UserAuthTokenSubscriber.php
@@ -26,37 +26,32 @@ class UserAuthTokenSubscriber implements EventSubscriberInterface {
     $request = \Drupal::request();
 
     $_ = $request->query->get('authtoken');
-    $auth_token = is_null($_) ? 'token_not_set' : $_;
+    $auth_token = is_null($_) ? '' : $_;
 
-    if ($auth_token != '' && $auth_token == 'qwe123') {
+    if (\Drupal::currentUser()->isAnonymous() && $auth_token != '') {
+      $uid = \Drupal::entityQuery('user')
+        ->condition('field_auth_token', $auth_token)
+        ->execute();
+      $uid = reset($uid);
+      if ($uid && $uid != 0) {
+        $user = User::load($uid);
+        if ($user) {
+          user_login_finalize($user);
+          db_update('users_field_data')
+            ->fields(array('login' => time()))
+            ->condition('uid', $uid)
+            ->execute();
+        }
+      }
+      else {
+        $message = t('Wrong token');
+        \Drupal::messenger()->addStatus($message);
+      }
 
-
-
-// TODO
-// - query field for token
-// - handle user/0
-// - make autologin
-//     $db_connection->query('
-//        SELECT uid
-//        FROM {user__field_auth_token}
-//        WHERE field_auth_token_value = @_1
-//     ', ['@_1' => $auth_token])
-//     ->fetchField();
-// ^^^^ MAKE IT FIELD QUERY ^^^^^^^^^^^^^^^
-//     $user = User::load($uid);
-//     user_login_finalize($user);
-//
-// - Find how to solve existing destination:
-//     local.dept/user/69/edit?destination=/admin/people
-
-
-
-      $destination = trim(str_replace($request->getQueryString(), '', $request->getRequestUri()), '?');
-      $response = new RedirectResponse($destination);
+      $url = Url::fromRoute('<front>');
+      $response = new RedirectResponse($url->toString());
       $response->send();
     }
-
-    drupal_set_message(t('@_1', ['@_1' => $auth_token]), 'error');
 
     $event->stopPropagation();
   }
